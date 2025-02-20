9fd93b200ebe3703e1adc38abd23beeca23a5223
李权泓 <951477037@qq.com>
2025-02-20 11:19:01 +0800

    chore: update ESLint configuration with additional ignores and rule adjustments

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,14 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   unocss: true,
   vue: true,
+  ignores: [
+    'src-tauri/Cargo.toml',
+    'src-tauri/target/*',
+    'vite-env.d.ts',
+  ],
+  rules: {
+    'no-console': 'off', // 禁用console警告
+    'unused-imports/no-unused-vars': 'off', // 禁用未使用导入警告
+    'no-unused-vars': 'off', // 禁用未使用变量警告
+  },
 })
