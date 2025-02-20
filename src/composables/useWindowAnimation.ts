dd3aa84d7ff4303f02775537118654233268a79f
李权泓 <951477037@qq.com>
2025-02-20 17:21:02 +0800

    feat: refactor window positioning and update UI styling

--- src/composables/useWindowAnimation.ts
+++ src/composables/useWindowAnimation.ts
@@ -1,16 +1,14 @@
 import { getCurrentWindow, LogicalSize, PhysicalPosition, primaryMonitor } from '@tauri-apps/api/window'
 
-export async function useWindowAnimation() {
+// 设置窗口位置的函数
+export async function setWindowPosition() {
   // 获取当前窗口
   const window = getCurrentWindow()
-
-  const windowWidth = 100
+  const windowWidth = 300
   const windowHeight = 100
 
   // 设置窗口大小
   await window.setSize(new LogicalSize(windowWidth, windowHeight))
-
-  // 获取主显示器信息
   const monitor = await primaryMonitor()
   if (!monitor)
     return
@@ -18,6 +16,11 @@ export async function useWindowAnimation() {
   const xPos = monitor.size.width - windowWidth * 2 - 50
   const yPos = 100
 
-  // 设置窗口位置
   await window.setPosition(new PhysicalPosition(xPos, yPos))
+}
+
+export async function useWindowAnimation() {
+  // 获取当前窗口
+  const window = getCurrentWindow()
+  console.log('window', window)
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,15 +2,26 @@
 defineOptions({
   name: 'IndexPage',
 })
+
+// 初始化设置窗口位置
+setWindowPosition()
+
 onMounted(async () => {
   useWindowAnimation()
 })
 </script>
 
 <template>
-  <div h-100vh w-100vh flex justify-end border-2 border-red-500>
-    <div data-tauri-drag-region bg-red-500 text-4xl>
-      <div data-tauri-drag-region i-carbon-campsite inline-block />
+  <div h-100vh w-screen flex justify-end>
+    <!-- 图标区域容器 -->
+    <div
+      data-tauri-drag-region bg-red-500 px-3 py-2 text-4xl transition-all duration-300 hover:bg-red-600
+    >
+      <div
+        data-tauri-drag-region
+        i-carbon-campsite
+        inline-block
+      />
     </div>
   </div>
 </template>
