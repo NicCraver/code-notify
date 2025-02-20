531a4a2bf8a5e54a646eeffc4d163aa8e7a94414
李权泓 <951477037@qq.com>
2025-02-20 16:42:14 +0800

    feat: 透明无边框并且关闭shadow效果

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,56 +1,18 @@
 <script setup lang="ts">
+import { getCurrentWindow } from '@tauri-apps/api/window'
+
 defineOptions({
   name: 'IndexPage',
 })
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
-
-const { t } = useI18n()
+onMounted(async () => {
+  await getCurrentWindow().setShadow(false)
+})
 </script>
 
 <template>
   <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        m-3 text-sm btn
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
+    <div data-tauri-drag-region h-80px w-80px bg-red-500 text-4xl>
+      <div data-tauri-drag-region i-carbon-campsite inline-block />
     </div>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: home
-</route>
