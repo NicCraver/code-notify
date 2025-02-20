f8908610d52d355f7b7f9242f3dde315a8853684
李权泓 <951477037@qq.com>
2025-02-20 13:17:28 +0800

    chore: update ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,15 @@ export default antfu(
   {
     unocss: true,
     formatters: true,
+    ignores: [
+      'src-tauri/Cargo.toml',
+      'src-tauri/target/*',
+      'vite-env.d.ts',
+    ],
+    rules: {
+      'no-console': 'off', // 禁用console警告
+      'unused-imports/no-unused-vars': 'off', // 禁用未使用导入警告
+      'no-unused-vars': 'off', // 禁用未使用变量警告
+    },
   },
 )
